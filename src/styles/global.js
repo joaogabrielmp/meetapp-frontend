9c7ae7ca48b77eb43bf3740ad879f4ca8fe79a86
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-13 11:13:35 -0300

    up

---
+++ src/styles/global.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+   @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+   * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  *:focus {
+    outline: 0;
+  }
+  html, body, #root {
+    height: 100%;
+    min-height: 100vh;
+    background: linear-gradient(180deg, #22202C 0%, #402845 100%);
+  }
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+  a {
+    text-decoration: none;
+  }
+  ul {
+    list-style: none;
+  }
+  button {
+    cursor: pointer;
+  }
+`;
