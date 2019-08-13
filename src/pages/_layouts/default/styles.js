4dc1d68e3f458ce7a892dc02070ba9ba1866cbc3
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-13 15:12:48 -0300

    up

---
+++ src/pages/_layouts/default/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(180deg, #22202c 0%, #402845 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
--- src/routes/index.js
+++ src/routes/index.js
@@ -13,6 +13,8 @@ export default function Routes() {
       <Route path="/register" component={SignUp} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/profile" component={Profile} isPrivate />
+
+      <Route path="/" component={() => <h1>Error 404</h1>} />
     </Switch>
   );
 }
