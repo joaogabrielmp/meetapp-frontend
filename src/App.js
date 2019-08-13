081f86d9d76e8cd9cd3f6d9515cc0bba72f3ec5c
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-13 14:02:59 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import history from './services/history';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <h1>Hello World</h1>
+      <Router history={history}>
+        <GlobalStyle />
+        <Routes />
+      </Router>
     </>
   );
 }
