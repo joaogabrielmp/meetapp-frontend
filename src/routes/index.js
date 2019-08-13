a0150e3394e6a49baa3b304ead0008354492b338
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-13 14:50:25 -0300

    up

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,8 +11,8 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
