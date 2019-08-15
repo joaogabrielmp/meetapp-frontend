cc08c44ff495a2af34313a68120a9da25c27f773
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-15 16:03:59 -0300

    up

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { MdAddCircleOutline } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+
+        <hr />
+
+        <Input name="oldPassword" type="password" placeholder="Senha atual" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">
+          <MdAddCircleOutline size={20} />
+          Salvar perfil
+        </button>
+      </Form>
+    </Container>
+  );
 }
