f3a9336123b37b4177bb6337a04ed96801310fe3
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-15 17:01:35 -0300

    up

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 import { MdAddCircleOutline } from 'react-icons/md';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
   return (
     <Container>
-      <Form>
+      <Form initialData={profile}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
 
