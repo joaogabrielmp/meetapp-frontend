4e7d813dd189f7e07112bedf1fdee2916152523f
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-15 17:11:45 -0300

    up

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { MdAddCircleOutline } from 'react-icons/md';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import { Container } from './styles';
 
 export default function Profile() {
   const profile = useSelector(state => state.user.profile);
+  const loading = useSelector(state => state.user.loading);
+  const dispatch = useDispatch();
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
-      <Form initialData={profile}>
+      <Form initialData={profile} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
 
@@ -27,8 +33,14 @@ export default function Profile() {
         />
 
         <button type="submit">
-          <MdAddCircleOutline size={20} />
-          Salvar perfil
+          {loading ? (
+            'Salvando...'
+          ) : (
+            <>
+              <MdAddCircleOutline size={20} />
+              Salvar perfil
+            </>
+          )}
         </button>
       </Form>
     </Container>
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -9,8 +9,10 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     margin-top: 30px;
+    align-items: flex-end;
 
     input {
+      width: 100%;
       background: rgba(0, 0, 0, 0.1);
       border: 0;
       border-radius: 4px;
@@ -36,6 +38,7 @@ export const Container = styled.div`
       height: 1px;
       background: rgba(255, 255, 255, 0.2);
       margin: 10px 0 20px;
+      width: 100%;
     }
 
     button {
@@ -43,7 +46,6 @@ export const Container = styled.div`
       justify-content: center;
       align-items: center;
       width: 150px;
-
       margin: 5px 0 0;
       height: 44px;
       background: #d44059;
@@ -56,6 +58,10 @@ export const Container = styled.div`
 
       &:hover {
         background: ${darken(0.08, '#d44059')};
+      }
+
+      svg {
+        margin-right: 10px;
       }
     }
   }
