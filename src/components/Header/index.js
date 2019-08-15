1018604f63cf54292ca96565ff781b32a91cf1d8
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-15 11:06:57 -0300

    up

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
+
+import { Container, Content, Profile, Button } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <Link to="/dashboard">
+            {/* DASHBOARD */}
+            <img src={logo} alt="Meetapp" />
+          </Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Joao Gabriel</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+          </Profile>
+
+          <Button>Sair</Button>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  background: #121212;
+  padding: 0 30px;
+`;
+
+export const Content = styled.div`
+  height: 64px;
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  aside {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  margin-right: 15px;
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #fff;
+    }
+
+    a {
+      display: block;
+      margin-top: 2px;
+      font-size: 12px;
+      color: #999;
+    }
+  }
+`;
+
+export const Button = styled.button`
+  height: 38px;
+  width: 70px;
+  background: #d44059;
+  color: #fff;
+  border: 0;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  transition: background 0.2s;
+
+  &:hover {
+    background: ${darken(0.08, '#D44059')};
+  }
+`;
