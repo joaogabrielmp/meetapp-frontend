3f909a55c76ec568aafeefe7ab5c19bf01e49d10
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-16 14:47:49 -0300

    up

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { MdAddCircleOutline, MdChevronRight } from 'react-icons/md';
+// import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Button, Meetup } from './styles';
+
+function handleNewMeetup() {}
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <h2>Meus meetups</h2>
+        <Button onClick={handleNewMeetup}>
+          <MdAddCircleOutline size={20} />
+          Meus meetups
+        </Button>
+      </header>
+
+      <ul>
+        <Meetup past>
+          <strong>Meetup de React Native</strong>
+          <span>24 de Junho, às 20h</span>
+          <MdChevronRight size={30} />
+        </Meetup>
+
+        <Meetup>
+          <strong>Meetup de NodeJS</strong>
+          <span>17 de Julho, às 13h</span>
+          <MdChevronRight size={30} />
+        </Meetup>
+
+        <Meetup>
+          <strong>Meetup de ReactJS</strong>
+          <span>30 de Agosto, às 20h</span>
+          <MdChevronRight size={30} />
+        </Meetup>
+      </ul>
+    </Container>
+  );
 }
