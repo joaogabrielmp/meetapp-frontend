40c9b74e035624fbb7802ee17e5f9ed202768ef0
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-19 13:16:38 -0300

    up

--- src/pages/Meetup/index.js
+++ src/pages/Meetup/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { Container } from './styles';
 
 export default function Meetup({ match }) {
   const { id } = match.params;
+  const meetup = useSelector(state => state.meetup);
 
   if (!id) {
     alert('new');
   } else {
     alert('edit');
   }
+
+  console.log(meetup);
 
   return <Container />;
 }
--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -4,7 +4,7 @@ import { persistReducer } from 'redux-persist';
 export default reducers => {
   const persistedReducer = persistReducer(
     {
-      key: 'meetapp',
+      key: 'meetapp-web',
       storage,
       whitelist: ['auth', 'user', 'meetup'],
     },
