c490a556beca84f3a152ebd79dc0f02ca8ae29e5
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-19 13:54:16 -0300

    up

---
+++ src/components/ImageInput/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { MdCameraAlt } from 'react-icons/md';
+import api from '~/services/api';
+
+import { Container } from './styles';
+
+export default function ImageInput() {
+  async function handleChange(e) {}
+
+  return <Container></Container>;
+}
