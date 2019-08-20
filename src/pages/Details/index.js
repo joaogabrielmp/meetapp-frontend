51c7a3a62d421e93b96fd61e14dcefd91311193f
joaogabrielmp <joaogabrielma@gmail.com>
2019-08-20 09:45:40 -0300

    up

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdModeEdit, MdDeleteForever } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, EditButton, CancelButton } from './styles';
+
+const meetup = [];
 
 export default function Details() {
-  return <Container>Details</Container>;
+  return (
+    <Container>
+      <header>
+        <h2>Meus meetups</h2>
+        <Link to={`/meetup/edit/${meetup.id}`}>
+          <EditButton>
+            <MdModeEdit size={20} />
+            Editar
+          </EditButton>
+        </Link>
+        <CancelButton>
+          <MdDeleteForever size={20} />
+          Cancelar
+        </CancelButton>
+      </header>
+    </Container>
+  );
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -5,3 +5,51 @@ export const Container = styled.div`
   max-width: 900px;
   margin: 30px auto;
 `;
+
+export const EditButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 168px;
+  margin: 5px 0 0;
+  height: 44px;
+  background: #4dbaf9;
+  color: #fff;
+  border: 0;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  transition: background 0.2s;
+
+  &:hover {
+    background: ${darken(0.08, '#4DBAF9')};
+  }
+
+  svg {
+    margin-right: 10px;
+  }
+`;
+
+export const CancelButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 168px;
+  margin: 5px 0 0;
+  height: 44px;
+  background: #d44059;
+  color: #fff;
+  border: 0;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  transition: background 0.2s;
+
+  &:hover {
+    background: ${darken(0.08, '#D44059')};
+  }
+
+  svg {
+    margin-right: 10px;
+  }
+`;
